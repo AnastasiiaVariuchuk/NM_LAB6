6e723e13f281d46e6f55aa63ae21c261f0a974bf
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-05-13 23:54:16 +0300

    update

--- main.py
+++ main.py
@@ -44,28 +44,26 @@ class FaceRecognition:
         while True:
             ret, frame = video_capture.read()
 
-            # Only process every other frame of video to save time
+            # Обробляється лише кожен другий кадр відео, щоб заощадити час
             if self.process_current_frame:
-                # Resize frame of video to 1/4 size for faster face recognition processing
+                # Встановлюємо розмір кадру відео до 1/4 для швидшої обробки розпізнавання обличчя
                 try:
                     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
                 except:
                     break
-                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-                # rgb_small_frame = small_frame[:, :, ::-1]
 
-                # Find all the faces and face encodings in the current frame of video
+                # Знаходимо всі обличчя та кодування облич у поточному кадрі відео
                 self.face_locations = face_recognition.face_locations(small_frame)
                 self.face_encodings = face_recognition.face_encodings(small_frame, self.face_locations)
 
                 self.face_names = []
                 for face_encoding in self.face_encodings:
-                    # See if the face is a match for the known face(s)
+                    # Перевірка, чи обличчя збігається з відомими обличчями
                     matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
                     name = "Unknown"
                     confidence = '???'
 
-                    # Calculate the shortest distance to face
+                    # Обчислимо найкоротшу відстань до грані
                     face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
 
                     best_match_index = np.argmin(face_distances)
@@ -77,27 +75,27 @@ class FaceRecognition:
 
             self.process_current_frame = not self.process_current_frame
 
-            # Display the results
+            # Відобразимо результати
             for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
-                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+                # Відновлюємо масштаб розташування облич, оскільки кадр, який ми виявили,
+                # був масштабований до 1/4 розміру
                 top *= 4
                 right *= 4
                 bottom *= 4
                 left *= 4
 
-                # Create the frame with the name
+                # Створюємо рамку з назвою
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
 
-            # Display the resulting image
+            # Відобразимо отримане зображення
             cv2.imshow('Face Recognition', frame)
 
-            # Hit 'q' on the keyboard to quit!
+            # Натисніть «q» на клавіатурі, щоб вийти!
             if cv2.waitKey(1) == ord('q'):
                 break
 
-        # Release handle to the webcam
         video_capture.release()
         cv2.destroyAllWindows()
 
